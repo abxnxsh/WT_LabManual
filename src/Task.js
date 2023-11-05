9493e363cd8a4197a52649580fdde5f7bba32f98
abxnxsh <91929479+abxnxsh@users.noreply.github.com>
2023-11-05 22:15:20 +0530

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,15 @@
+// Task.js
+import React from 'react';
+
+const Task = ({ task, onDelete }) => {
+  return (
+    <div className="task">
+      <h3>{task.text}</h3>
+      <button className="btn" onClick={() => onDelete(task.id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Task;
